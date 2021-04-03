c14b2395c7870983770ef2928e72b2db5b138d15
ckohen <chaikohen@gmail.com>
2021-04-03 15:06:54 -0700

    feat(PortAuthority): add update and reboot commands

--- src/rafts/portAuthority/commands/index.js
+++ src/rafts/portAuthority/commands/index.js
@@ -6,5 +6,7 @@ commands.eval = require('./eval');
 commands.ping = require('./ping');
 commands.echo = require('./echo');
 commands.img = require('./img');
+commands.reboot = require('./reboot');
+commands.update = require('./update');
 
 module.exports = commands;
