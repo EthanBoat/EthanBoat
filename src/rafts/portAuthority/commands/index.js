22758e6c630935b1071a6ce8c9f4ed5bd6b306b9
spicyebob <81609314+spicyebob@users.noreply.github.com>
2021-04-03 15:42:46 -0700

    Hello command addition

--- src/rafts/portAuthority/commands/index.js
+++ src/rafts/portAuthority/commands/index.js
@@ -6,5 +6,6 @@ commands.eval = require('./eval');
 commands.ping = require('./ping');
 commands.echo = require('./echo');
 commands.img = require('./img');
+commands.hello = require('./hello')
 
 module.exports = commands;
