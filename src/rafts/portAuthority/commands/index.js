b8b324ec2c5740b34ef5ad8f1495a53e57312d04
spicyebob <81609314+spicyebob@users.noreply.github.com>
2021-04-05 00:28:36 -0700

    feat(PortAuthority): add space and hello command (#12)
    * Hello command addition
    
    * Co-authored-by: Markens123 <Markens123@users.noreply.github.com>
    
    * added space command
    
    * fixed lint
    
    * chore: remove package.json dep
    
    * Fixed package lock
    
    * Update src/boat.js
    
    Co-authored-by: ckohen <chaikohen@gmail.com>

--- src/rafts/portAuthority/commands/index.js
+++ src/rafts/portAuthority/commands/index.js
@@ -6,6 +6,8 @@ commands.eval = require('./eval');
 commands.ping = require('./ping');
 commands.echo = require('./echo');
 commands.img = require('./img');
+commands.hello = require('./hello');
+commands.space = require('./space');
 commands.reboot = require('./reboot');
 commands.update = require('./update');
 
